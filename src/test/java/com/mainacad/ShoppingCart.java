a43e6b88f49f207718f05ff06ab2d11471e4569f
Yuriy Khortashko <yuriy.khortashko@gmail.com>
2017-10-06 01:42:44 +0300

    Commented tests, left 4 tests only

--- src/test/java/com/mainacad/ShoppingCart.java
+++ src/test/java/com/mainacad/ShoppingCart.java
@@ -78,198 +78,198 @@ public class ShoppingCart extends TestBase {
     }
 
 
-    @Test
-    public void viewShoppingCartChrome11() throws MalformedURLException {
-        System.out.println("Starting chrome test11");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.chrome());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing chrome test11");
-    }
-
-    @Test
-    public void viewShoppingCartChrome12() throws MalformedURLException {
-        System.out.println("Starting chrome test12");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.chrome());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing chrome test12");
-    }
-
-
-    @Test
-    public void viewShoppingCartFirefox11() throws MalformedURLException {
-        System.out.println("Starting ff test11");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing ff test11");
-    }
-
-    @Test
-    public void viewShoppingCartFirefox12() throws MalformedURLException {
-        System.out.println("Starting ff test12");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing ff test12");
-    }
-    @Test
-    public void viewShoppingCartChrome21() throws MalformedURLException {
-        System.out.println("Starting chrome test21");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.chrome());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing chrome test21");
-    }
-
-    @Test
-    public void viewShoppingCartChrome22() throws MalformedURLException {
-        System.out.println("Starting chrome test22");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.chrome());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing chrome test22");
-    }
-
-
-    @Test
-    public void viewShoppingCartFirefox21() throws MalformedURLException {
-        System.out.println("Starting ff test21");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing ff test21");
-    }
-
-    @Test
-    public void viewShoppingCartFirefox22() throws MalformedURLException {
-        System.out.println("Starting ff test22");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing ff test22");
-    }
-    @Test
-    public void viewShoppingCartChrome31() throws MalformedURLException {
-        System.out.println("Starting chrome test31");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.chrome());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing chrome test31");
-    }
-
-    @Test
-    public void viewShoppingCartChrome32() throws MalformedURLException {
-        System.out.println("Starting chrome test32");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.chrome());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing chrome test32");
-    }
-
-
-    @Test
-    public void viewShoppingCartFirefox31() throws MalformedURLException {
-        System.out.println("Starting ff test31");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing ff test31");
-    }
-
-    @Test
-    public void viewShoppingCartFirefox32() throws MalformedURLException {
-        System.out.println("Starting ff test32");
-
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-
-        ShoppingCartPage catalogPage =
-                new WelcomePage(driver).open()
-                        .enterStore()
-                        .clickOnShoppingCart();
-
-        Assert.assertTrue(catalogPage.isPageDisplayed());
-        System.out.println("Finishing ff test32");
-    }
+//    @Test
+//    public void viewShoppingCartChrome11() throws MalformedURLException {
+//        System.out.println("Starting chrome test11");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.chrome());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing chrome test11");
+//    }
+//
+//    @Test
+//    public void viewShoppingCartChrome12() throws MalformedURLException {
+//        System.out.println("Starting chrome test12");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.chrome());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing chrome test12");
+//    }
+//
+//
+//    @Test
+//    public void viewShoppingCartFirefox11() throws MalformedURLException {
+//        System.out.println("Starting ff test11");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.firefox());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing ff test11");
+//    }
+//
+//    @Test
+//    public void viewShoppingCartFirefox12() throws MalformedURLException {
+//        System.out.println("Starting ff test12");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.firefox());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing ff test12");
+//    }
+//    @Test
+//    public void viewShoppingCartChrome21() throws MalformedURLException {
+//        System.out.println("Starting chrome test21");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.chrome());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing chrome test21");
+//    }
+//
+//    @Test
+//    public void viewShoppingCartChrome22() throws MalformedURLException {
+//        System.out.println("Starting chrome test22");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.chrome());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing chrome test22");
+//    }
+//
+//
+//    @Test
+//    public void viewShoppingCartFirefox21() throws MalformedURLException {
+//        System.out.println("Starting ff test21");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.firefox());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing ff test21");
+//    }
+//
+//    @Test
+//    public void viewShoppingCartFirefox22() throws MalformedURLException {
+//        System.out.println("Starting ff test22");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.firefox());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing ff test22");
+//    }
+//    @Test
+//    public void viewShoppingCartChrome31() throws MalformedURLException {
+//        System.out.println("Starting chrome test31");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.chrome());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing chrome test31");
+//    }
+//
+//    @Test
+//    public void viewShoppingCartChrome32() throws MalformedURLException {
+//        System.out.println("Starting chrome test32");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.chrome());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing chrome test32");
+//    }
+//
+//
+//    @Test
+//    public void viewShoppingCartFirefox31() throws MalformedURLException {
+//        System.out.println("Starting ff test31");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.firefox());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing ff test31");
+//    }
+//
+//    @Test
+//    public void viewShoppingCartFirefox32() throws MalformedURLException {
+//        System.out.println("Starting ff test32");
+//
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+//                DesiredCapabilities.firefox());
+//
+//        ShoppingCartPage catalogPage =
+//                new WelcomePage(driver).open()
+//                        .enterStore()
+//                        .clickOnShoppingCart();
+//
+//        Assert.assertTrue(catalogPage.isPageDisplayed());
+//        System.out.println("Finishing ff test32");
+//    }
 
 
 
