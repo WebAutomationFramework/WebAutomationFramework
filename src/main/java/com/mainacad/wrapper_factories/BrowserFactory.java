9281d33c13ac83e65d4f44ebc5383db75e4ffafb
berlinisimus <berlinisimus.92@gmail.com>
2017-10-29 14:23:06 +0200

    completed jenkins run feature

--- src/main/java/com/mainacad/wrapper_factories/BrowserFactory.java
+++ src/main/java/com/mainacad/wrapper_factories/BrowserFactory.java
@@ -1,10 +1,13 @@
 package com.mainacad.wrapper_factories;
 
-import com.mainacad.helpers.PropertyWorker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 /**
  * Created by Olexiy on 9/13/2017.
@@ -12,24 +15,46 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class BrowserFactory {
     private static WebDriver driver = null;
 
-    public static WebDriver initDriver() {
-        String driverType = PropertyWorker.getProperty();
+    private static final String CHROME = "chrome";
+    private static final String FIREFOX = "firefox";
+    private static final String REMOTE_CHROME = "remote-chrome";
+    private static final String REMOTE_FIREFOX = "remote-firefox";
 
-        switch (driverType) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
+    private static final String REMOTE_DRIVER_HUB_URL = "http://localhost:4444/wd/hub";
+
+    public static WebDriver getInstance(String browser) throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (driver == null) {
+            if (CHROME.equals(browser)) {
+
+                capabilities.chrome();
+                driver = new ChromeDriver(capabilities);
+
+            } else if (FIREFOX.equals(browser)) {
+
+                capabilities.firefox();
                 driver = new FirefoxDriver();
-                break;
-            default:
-                driver = new ChromeDriver();
-                break;
+
+            } else if (REMOTE_CHROME.equals(browser)) {
+
+                capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+                driver = new RemoteWebDriver(new URL(REMOTE_DRIVER_HUB_URL), capabilities);
+
+            } else if (REMOTE_FIREFOX.equals(browser)) {
+
+                capabilities.setCapability(CapabilityType.BROWSER_NAME, "firefox");
+                driver = new RemoteWebDriver(new URL(REMOTE_DRIVER_HUB_URL), capabilities);
+
+            } else {
+                throw new Exception("Invalid browser property");
+            }
         }
         return driver;
     }
 
     public static void closeDriver() {
         driver.quit();
+        driver = null;
     }
 }
--- src/test/java/com/mainacad/TestBase.java
+++ src/test/java/com/mainacad/TestBase.java
@@ -1,27 +1,36 @@
 package com.mainacad;
 
+import com.mainacad.helpers.PropertyLoader;
 import com.mainacad.wrapper_factories.BrowserFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     protected WebDriver driver;
+    private String timeout;
     public String ProuductIdStr = "FL-DLH-02";
 
     @BeforeMethod
-    public void setUp(){
-        driver = BrowserFactory.initDriver();
-//        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    @Parameters({ "browserName" })
+    public void setUp(String browserName) throws Exception {
+        timeout = PropertyLoader.loadProperty("implicit-timeout");
+
+        driver = BrowserFactory.getInstance(browserName);
+        System.out.println(driver.toString());
+
+        driver.manage().timeouts().implicitlyWait(Integer.valueOf(timeout), TimeUnit.SECONDS);
+
+        System.out.println("Running tests on:" + browserName);
     }
 
     @AfterMethod
-    public void quitDriver(){
-        BrowserFactory.closeDriver();
+    public void quitDriver() throws InterruptedException {
+        System.out.println("Closing browser");
+        if(driver!= null){
+            BrowserFactory.closeDriver();
+        }
     }
 }
